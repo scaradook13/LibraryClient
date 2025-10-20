a50068cf55249bf53e979d2b6c8b515e71113b92
scaradook13 <mythicalwarrior132006@gmail.com>
2025-10-20 21:49:04 +0800

    new api

--- src/api/baseService.js
+++ src/api/baseService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class BaseService {
     constructor() {
         this.api = axios.create({
-            baseURL: 'https://library-api-sigma-weld.vercel.app/',
+            baseURL: 'https://library-api-ochre.vercel.app',
             withCredentials: true,
         });
     }
